0df2aa6475c6aaad6c1fbe311ac6d174b2d6a00b
VinodTessrac <vinodh.a@tessrac.com>
2022-06-09 20:21:41 +0530

    app commented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import HomePageMixin from './pages/HomePage';
 import reportWebVitals from './reportWebVitals';
 

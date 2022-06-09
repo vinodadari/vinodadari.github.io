7e3951134ee5162f4af1c42de1481ac55983d696
VinodTessrac <vinodh.a@tessrac.com>
2022-06-09 11:25:12 +0530

    navbar - inprogress

--- src/layouts/PortfolioLayout.js
+++ src/layouts/PortfolioLayout.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const PortfolioLayout = (props) => {
     return (
-        <div className="app1">
-            I am a PortfolioLayout
+        <div className="portfolio_root_body">
+            <header className="navigation_bar">
+                <nav>
+                    <div className="logo">
+                        <a href="#">I am logo</a>
+                    </div>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Experiance</a></li>
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Resume</a></li>
+                    </ul>
+                </nav>
+            </header>
             {React.createElement(props.component)}
         </div>
     )

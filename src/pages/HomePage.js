7eaf91bc4509fe2747beb5d455aec9b829059f89
VinodTessrac <vinodh.a@tessrac.com>
2022-06-10 13:25:18 +0530

    about section - scss changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,29 +5,31 @@ const PortfolioLayout = React.lazy(() => import("../layouts/PortfolioLayout"))
 
 const HomePage = () => {
     return (
-        <>
+        <div>
             <main className="HomePage_content">
                 <section className="about_section" id="about">
-                    <div className="highlight ">
-                        <h1>Hi, my name is</h1>
-                    </div>
-                    <div className="big_heading">
-                        <h2>Vinodh Kumar Adari.</h2>
-                    </div>
-                    <div className="big_heading">
-                        <h3>I build things for the web.</h3>
-                    </div>
-                    <div className="description">
-                        <p>
-                            I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at <a href="https://tessrac.com/" rel="noreferrer" target="_blank" className="highlight_btn">Tessrac</a>
-                        </p>
-                    </div>
-                    <div className="DeveloperActivity">
-                        <DeveloperActivity />
-                    </div>
+                    <span>
+                        <div className="highlight ">
+                            <h1>Hi, my name is</h1>
+                        </div>
+                        <div className="big_heading">
+                            <h2>Vinodh Kumar Adari.</h2>
+                        </div>
+                        <div className="big_heading">
+                            <h3>I build things for the web.</h3>
+                        </div>
+                        <div className="description">
+                            <p>
+                                I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at <a href="https://tessrac.com/" rel="noreferrer" target="_blank" className="highlight_text">Tessrac</a>
+                            </p>
+                        </div>
+                        <div className="DeveloperActivity">
+                            <DeveloperActivity />
+                        </div>
+                    </span>
                 </section>
             </main>
-        </>
+        </div>
     )
 }
 

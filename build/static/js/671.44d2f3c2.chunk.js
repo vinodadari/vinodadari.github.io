59d9a4804cbf00bef46852e3e73b1fab083ab9a1
VinodTessrac <vinodh.a@tessrac.com>
2023-05-14 16:09:08 +0530

    build dir removed

--- build/static/js/671.44d2f3c2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkitsvinodadari_portfolio=self.webpackChunkitsvinodadari_portfolio||[]).push([[671],{671:function(o,n,t){t.r(n);var e=t(791),i=t(184),r=e.lazy((function(){return t.e(367).then(t.bind(t,758))}));n.default=function(o){return(0,i.jsxs)("div",{className:"portfolio_root_body",children:[(0,i.jsx)(r,{}),e.createElement(o.component)]})}}}]);
-//# sourceMappingURL=671.44d2f3c2.chunk.js.map
